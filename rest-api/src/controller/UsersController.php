a3982a8f16a8c91d9ecfcbcc52c0338ba123a63e
maskadev <sidkiyouness@gmail.com>
2022-05-09 22:24:07 +0100

    sign in form added

--- rest-api/src/controller/UsersController.php
+++ rest-api/src/controller/UsersController.php
@@ -15,18 +15,19 @@ class UsersController extends JwtController{
 	{
 				if($_SERVER['REQUEST_METHOD'] == 'POST')
 				{
+					ini_set('display_errors', 1);
+					ini_set('display_startup_errors', 1);
+					error_reporting(E_ALL);
 					require_once('src/models/authentication.php');
 					require_once('src/config/Header.php');
 						$auth =  new Authentication();
-						$user_id = json_decode(file_get_contents("php://input"));
-						$result = $auth->signin($user_id->user_id);
-						if($result){
-							require_once('src/security/jwt_handler.php');
-							$jwt = new JwtController();
-							$token = $jwt->authorization();
-							echo Authentication::message('Logedin!', $token, $result ,false);
+						$email = $_POST['email'];
+						$password = $_POST['password'];
+						$result = $auth->signin($email, $password);
+						if(!empty($result)){
+							echo Authentication::message('Logedin!', $result['id'] ,false);
 						}else {
-							echo Authentication::message('User not exist please try to sign up first', null, null, true);
+							echo Authentication::message('invalide email or password', null, null, true);
 						}
 				}
 	}
@@ -40,22 +41,13 @@ class UsersController extends JwtController{
 		if($_SERVER['REQUEST_METHOD'] == 'POST')
 		{
 			// $email = json_decode(file_get_contents("php://input"));
-			// $full_name = json_decode(file_get_contents("php://input"));
-			// $password = json_decode(file_get_contents("php://input"));
-			// $password_confirmation = json_decode(file_get_contents("php://input"));
 			$full_name = $_POST['full_name'];
 			$email = $_POST['email'];
 			$password = $_POST['password'];
 			if(!empty($email) && !empty($full_name) && !empty($password)){
-				
-				// if($password->password == $password_confirmation->password_confirmation) {
 					$auth =  new Authentication();
 					$result = $auth->signup($full_name, $email, $password);
 					echo $result;
-				// }else {
-				// 	echo Authentication::message('password not the same', null, null, false);
-				// }
-
 			}else {
 				echo Authentication::message('Empty InputField!', null, null, true);
 			}
--- rest-api/src/models/authentication.php
+++ rest-api/src/models/authentication.php
@@ -2,26 +2,32 @@
 
 class Authentication {
 
-
     public function signup($full_name, $email, $password) {
         require_once('connection.php');
         $db = new Database();
-        $query = $db->connection()->prepare("INSERT INTO `users`(`full_name`, `email`, `password`) VALUES (:full_name, :email, :password)");
-        $result = $query->execute([':full_name' => $full_name, ':email' => $email, ':password' => $password]);
-        if($result)
+        $userExist = $db->connection()->prepare("SELECT `email` FROM `users` WHERE email=?")->execute([$email]);
+        if(empty($userExist))
         {
-            return Authentication::message('user has been added!', 0, false);
+            $query = $db->connection()->prepare("INSERT INTO `users`(`full_name`, `email`, `password`) VALUES (:full_name, :email, :password)");
+            $result = $query->execute([':full_name' => $full_name, ':email' => $email, ':password' => $password]);
+            if($result)
+            {
+                return Authentication::message('user has been added!', 0, false);
+            }else {
+                return Authentication::message('user has not been added!', null, true);
+            }
         }else {
-            return Authentication::message('user has not been added!', null, true);
+            return Authentication::message('usr already exist', null, false);
         }
         
     }
 
-    public function signin($id) {
+    public function signin($email, $password) {
         require_once('connection.php');
         $db = new Database();
-        $result = $db->checkUserExist($id);
-        return $result['id'];
+        $result = $db->connection()->prepare("SELECT * FROM `users` WHERE email=? AND password=?");
+        $result->execute([$email, $password]); 
+        return $result->fetch(PDO::FETCH_ASSOC);
     }
 
     public static function message($content, $user_id, $status) {
