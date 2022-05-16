10eea1b8c0aa688780392a0360773b6d569bae29
maskadev <sidkiyouness@gmail.com>
2022-05-16 22:31:54 +0100

    add data types for athentication and navbar Links

---
+++ app/src/types/index.ts
@@ -0,0 +1,13 @@
+
+
+export interface  NavBarLinks { 
+    link : string,
+    title : string
+}
+
+export interface Signinput {
+    id:string,
+    label:string,
+    placeholder:string,
+    type:string
+}
