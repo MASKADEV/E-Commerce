3137129bab387cf0aadc4b31d4e6286b53adbd98
maskadev <sidkiyouness@gmail.com>
2022-05-16 22:31:23 +0100

    add home page

---
+++ app/src/pages/Home/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Authentication from '../authentication'
+
+const Home = () => {
+  return (
+    <>
+      <Authentication />
+    </>
+  )
+}
+
+export default Home
