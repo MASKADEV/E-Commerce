bf0f356ac6fc224384dbe5efcd3e2a6851e6aa19
maskadev <sidkiyouness@gmail.com>
2022-05-19 14:47:37 +0100

    add page not found path

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import Layout from './components/Commun/Layouts';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import PageNotFound from './pages/404';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path='' element={<Layout>{ <Home/>}</Layout>}/>
         <Route path='/home' element={<Layout>{ <Home/>}</Layout>}/>
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
     </Router>
   );
