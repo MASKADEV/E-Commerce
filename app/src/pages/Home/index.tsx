00d0554bddf25bd6fc6c4e670c51bc91c5dfa1cb
maskadev <sidkiyouness@gmail.com>
2022-05-19 14:51:56 +0100

    add auth props and Panier Props, also usestate Panier props

--- app/src/pages/Home/index.tsx
+++ app/src/pages/Home/index.tsx
@@ -1,12 +1,22 @@
-import React from 'react'
-import PageNotFound from '../404'
-import Authentication from '../authentication'
+import React from 'react';
+import ProductCard from '../../components/ui/ProductCard/ProductCard';
+import Authentication from '../authentication';
 
 const Home = () => {
   return (
-    <>
-      <Authentication />
-    </>
+      <div className='flex flex-row overflow-x-hidden'>
+        <div className='w-[25rem] md:flex hidden'></div>
+        <div className='grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-5 mb-3'>
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+        </div>
+      </div>
   )
 }
 
--- app/src/types/index.ts
+++ app/src/types/index.ts
@@ -10,5 +10,22 @@ export interface CustomInputProps {
     label:string,
     placeholder:string,
     type:string,
-    useRef : React.LegacyRef<HTMLInputElement> | undefined
+    useRef : React.LegacyRef<HTMLInputElement> | undefined,
+}
+
+export interface AuthProps {
+    show : boolean,
+    setShow : React.SetStateAction<any>
+}
+
+export interface AddProductsCardsProps {
+    title : string,
+    categorie : string,
+    price : number,
+    image_url : string,
+}
+
+export interface showPanieProps {
+    showPanier : boolean,
+    setShowPanier : React.SetStateAction<any>,
 }
