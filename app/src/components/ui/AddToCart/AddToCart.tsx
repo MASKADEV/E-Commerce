e2d9aea621d9f9e32b81dd2eb9e60a865f6961bd
maskadev <sidkiyouness@gmail.com>
2022-05-20 17:51:59 +0100

    change NavBar Props

--- app/src/components/ui/AddToCart/AddToCart.tsx
+++ app/src/components/ui/AddToCart/AddToCart.tsx
@@ -1,9 +1,8 @@
 import React from 'react'
-import { showPanieProps } from '../../../types'
+import { navBarProps } from '../../../types'
 import { CloseIcon } from '../../icons/close-icon'
-import Product from '../../models/Product'
 
-const AddToCart:React.FC<showPanieProps> = ({showPanier, setShowPanier}) => {
+const AddToCart:React.FC<navBarProps> = ({showPanier, setShowPanier}) => {
   return (
     <div className={`fixed h-full ${!showPanier ? ' -right-[30rem]' : ' right-0'} top-0 w-[25rem] z-30 duration-300 ease-out bg-navBar-bg text-white`}>
         <CloseIcon onClick={() => {setShowPanier(!showPanier)}} className=' absolute top-9 right-8 h-6 cursor-pointer'/>
