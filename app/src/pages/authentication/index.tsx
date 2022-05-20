b03061e06eb21fb04e3308404a533739a8e2403d
maskadev <sidkiyouness@gmail.com>
2022-05-20 17:52:45 +0100

    clean the code by deleing some spacing

--- app/src/pages/authentication/index.tsx
+++ app/src/pages/authentication/index.tsx
@@ -1,15 +1,15 @@
 import React, { useState } from 'react';
+import { AuthProps } from '../../types';
 import Signin from './Signin/Signin';
 import Signup from './Signup/Signup';
 
-const Authentication = () => {
+const Authentication:React.FC<AuthProps> = ({showAuth, setAuth}) => {
 
   const [show, setshow] = useState<boolean>(false);
-
   return (
-    <div className='absolute top-0 left-0 h-[100%] w-[100%] flex items-center justify-center'>
+    <div className='fixed z-20 top-0 left-0 h-[100%] w-[100%] flex items-center justify-center'>
       {
-        show === false ? <Signin show ={show} setShow={setshow}/> : <Signup show ={show} setShow={setshow}/>
+        show === false ? <Signin show ={show} showAuth={showAuth} setAuth={setAuth} setShow={setshow}/> : <Signup show ={show}  showAuth={showAuth} setAuth={setAuth}  setShow={setshow}/>
       }
     </div>
   )
