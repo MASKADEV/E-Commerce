15b101ab6bd4b02c0f7096d30357ddeda35a6213
maskadev <sidkiyouness@gmail.com>
2022-05-20 17:50:12 +0100

    change color extends

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -3,21 +3,22 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    colors: {
-      transparent: 'transparent',
-      'primary-bg': '#171717',
-      'navBar-bg': '#252525',
-      'sideBar-bg': '#212121',
-      'white': '#FFFFFF',
-      'hover-bg' :'#323232',
-      'main-color' :'#25B37E',
-      'gray' : '#A8A8A8'
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        'primary-bg': '#171717',
+        'navBar-bg': '#252525',
+        'sideBar-bg': '#212121',
+        'white': '#FFFFFF',
+        'hover-bg' :'#323232',
+        'main-color' :'#25B37E',
+        'gray' : '#A8A8A8'
+      },
+      backdropFilter: {
+        'none': 'none',
+        'blur': 'blur(20px)',
+      },
     },
-    backdropFilter: {
-      'none': 'none',
-      'blur': 'blur(20px)',
-    },
-    extend: {},
   },
   plugins: [],
 }
