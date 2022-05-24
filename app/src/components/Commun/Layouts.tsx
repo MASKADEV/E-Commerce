514b339ea536a84ab2267bee0e0ddd9c456fa95c
maskadev <sidkiyouness@gmail.com>
2022-05-24 10:10:23 +0100

    show profile page

--- app/src/components/Commun/Layouts.tsx
+++ app/src/components/Commun/Layouts.tsx
@@ -8,11 +8,12 @@ export default function Layout({ children } : any) {
 
   const [showPanie, setShowPanie] = useState<boolean>(false);
   const [showAuth, setAuth] = useState<boolean>(false);
+  const [showProfile, setProfile] = useState<boolean>(false);
 
   return (
     <main className='font-[Poppins] w-screen h-screen  bg-primary-bg'>
       <header>
-        <NavBar  showAuth={showAuth} setAuth={setAuth} showPanier={showPanie} setShowPanier={setShowPanie}/>
+        <NavBar showProfile={showProfile} setProfile={setProfile} showAuth={showAuth} setAuth={setAuth} showPanier={showPanie} setShowPanier={setShowPanie}/>
         <AddToCart showPanier={showPanie} setShowPanier={setShowPanie} />
         {showAuth && <Authentication showAuth={showAuth} setAuth={setAuth} /> }
       </header>
