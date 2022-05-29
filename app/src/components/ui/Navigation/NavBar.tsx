7368672a102b78c5be9e1aa869e0341e7fd82cc8
maskadev <sidkiyouness@gmail.com>
2022-05-29 17:25:16 +0100

    click on logo to go home

--- app/src/components/ui/Navigation/NavBar.tsx
+++ app/src/components/ui/Navigation/NavBar.tsx
@@ -13,7 +13,7 @@ const NavBar:React.FC<navBarProps> = ({showProfile, setProfile, showPanier, setS
   return (
     <div className='fixed z-20 h-[70px] bg-second-bg text-white w-full shadow-md bg-navBar-bg'>
         <div className='md:px-[7rem] px-10 mx-auto flex flex-row justify-between items-center h-full'>
-          <h1 className='md:text-2xl font-bold text-main-color'>BRAND.</h1>
+          <Link to="/" className=' cursor-pointer md:text-2xl font-bold text-main-color'>BRAND.</Link>
           <nav className='md:w-[30%] w-[70%] flex flex-row justify-end items-center'>
             <button  className='md:mr-[1rem] mr-3'>
               <SearchIcon className='md:h-[20px] h-[25px]'/>
