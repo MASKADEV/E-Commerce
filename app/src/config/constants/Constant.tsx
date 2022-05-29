a0956721ba1b076e588e386dc454102a322b67f0
maskadev <sidkiyouness@gmail.com>
2022-05-29 17:25:30 +0100

    update url path

--- app/src/config/constants/Constant.tsx
+++ app/src/config/constants/Constant.tsx
@@ -1,6 +1,6 @@
 
 class GlobalVarialble {
-     static url: string = 'http://localhost/e-commerce/rest-api/';
+     static url: string = 'http://localhost/e_commerce_fill_rouge/rest-api/';
 }
 
 export default GlobalVarialble;
