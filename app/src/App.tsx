c6275cd3b4e018187241d0e3f9c99b496dcbd3e4
Youness <sidkiyouness@gmail.com>
2022-06-01 23:26:39 +0100

    add supports paths

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -4,10 +4,13 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import PageNotFound from './pages/404';
 import Profile from './pages/Profile';
-import { Provider } from 'react-redux';
-import store from './store/store';
+// import { Provider } from 'react-redux';
+// import store from './store/store';
 import Layout from './components/commun/Layouts';
 import Dashboard from './pages/Admin/Dashboard';
+import FAQ from './pages/Support/faq/FAQ';
+import Terms from './pages/Support/terms/Terms';
+import Help from './pages/Support/help/Help';
 
 const  App:React.FC = () => {
   return (
@@ -16,6 +19,9 @@ const  App:React.FC = () => {
         <Route path='' element={<Layout>{ <Home/>}</Layout>}/>
         <Route path='/home' element={<Layout>{ <Home/>}</Layout>}/>
         <Route path='/admin' element={<Layout>{ <Dashboard/>}</Layout>}/>
+        <Route path='/faq' element={<Layout>{ <FAQ/>}</Layout>}/>
+        <Route path='/terms' element={<Layout>{ <Terms/>}</Layout>}/>
+        <Route path='/help' element={<Layout>{ <Help/>}</Layout>}/>
         <Route path='/profile' element={<Layout>{ <Profile /> }</Layout>}/>
         <Route path="*" element={<PageNotFound />} />
       </Routes>
@@ -23,12 +29,12 @@ const  App:React.FC = () => {
   );
 }
 
-const AppWrapper = () => {
-  return (
-    <Provider store={store}>
-      <App />
-    </Provider>
-  )
-}
+// const AppWrapper = () => {
+//   return (
+//     <Provider store={store}>
+//       <App />
+//     </Provider>
+//   )
+// }
 
-export default AppWrapper;
+export default App;
