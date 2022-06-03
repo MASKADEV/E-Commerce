feec588da3f4bb7ef3244d4bba083fa1d39fdf27
Youness <sidkiyouness@gmail.com>
2022-06-03 21:36:12 +0100

    add header for autorization

--- rest-api/src/config/Header.php
+++ rest-api/src/config/Header.php
@@ -2,4 +2,5 @@
 
 header('Access-Control-Allow-Origin: *');
 header('Content-type: application/json; charset=UTF-8');
-header('Access-Control-Allow-Methods: POST, GET, DELETE');
+header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT');
+header("Access-Control-Allow-Headers: Access-Control-Allow-Origin, Access-Control-Allow-Headers, Access-Control-Allow-Credentials, Content-Type, Access-Control-Allow-Methods, Authorization, X-Requested-With");
