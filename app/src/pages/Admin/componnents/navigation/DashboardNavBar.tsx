8a79277b1ef9fc0105eb9d463bb0ee33a6a81665
Youness <sidkiyouness@gmail.com>
2022-06-03 21:33:53 +0100

    add product form and edit form

--- app/src/pages/Admin/componnents/navigation/DashboardNavBar.tsx
+++ app/src/pages/Admin/componnents/navigation/DashboardNavBar.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { HelpIcon } from '../../../../components/icons/help-icon'
-import { HomeIcon } from '../../../../components/icons/home-icon'
 import ManagmentIcon from '../../../../components/icons/managment-icon'
 import OrdersIcon from '../../../../components/icons/orders-icon'
 import StaticsIcon from '../../../../components/icons/statitcs-icon'
@@ -11,36 +10,36 @@ const DashboardNavBar:React.FC<DashboardNavProps> = ({switchMenu, setswitchMenu}
     <div className='flex flex-row items-center justify-between w-[80%] mx-auto'>
         <div className='text-white font-medium md:text-xl text-sm'>
             <div>
-                <div className='flex flex-row items-center'>
-                    <StaticsIcon className='h-[1.5rem] mr-2 md:flex hidden' />
-                    <button className='m-0 p-0' onClick={()=>{setswitchMenu('home')}}>Analytics</button>
+                <div  onClick={()=>{setswitchMenu('home')}} className=' cursor-pointer flex flex-row items-center'>
+                    <StaticsIcon className='h-[1.5rem] mr-2' />
+                    <h1 className='md:flex hidden'>Analytics</h1>
                 </div>
                 <div className={`h-1 ${switchMenu === 'home' ? 'bg-white' : 'bg-transparent'} mt-2`}/>
             </div>
         </div>
         <div className='text-white font-medium md:text-xl text-sm'>
             <div>
-                <div className='flex flex-row items-center'>
-                    <ManagmentIcon className='h-[1.5rem] mr-2 md:flex hidden'/>
-                    <button onClick={()=>{setswitchMenu('managment')}}>Managment</button>
+                <div onClick={()=>{setswitchMenu('managment')}} className=' cursor-pointer flex flex-row items-center'>
+                    <ManagmentIcon className='h-[1.5rem] mr-2 '/>
+                    <h1 className='md:flex hidden'>Managment</h1>
                 </div>
                 <div className={`h-1 ${switchMenu === 'managment' ? 'bg-white' : 'bg-transparent'} mt-1`}/>
             </div>
         </div>
         <div className='text-white font-medium md:text-xl text-sm'>
             <div>
-                <div className='flex flex-row items-center'>
-                    <OrdersIcon className='h-[1.5rem] mr-2 md:flex hidden'/>
-                    <button onClick={()=>{setswitchMenu('orders')}}>Orders</button>
+                <div onClick={()=>{setswitchMenu('orders')}} className=' cursor-pointer flex flex-row items-center'>
+                    <OrdersIcon className='h-[1.5rem] mr-2'/>
+                    <h1 className=' md:flex hidden'>Orders</h1>
                 </div>
                 <div className={`h-1 ${switchMenu === 'orders' ? 'bg-white' : 'bg-transparent'} mt-1`}/>
             </div>
         </div>
         <div className='text-white font-medium md:text-xl text-sm'>
             <div>
-                <div className='flex flex-row items-center'>
-                    <HelpIcon className='h-[1.5rem] mr-2 md:flex hidden'/>
-                    <button onClick={()=>{setswitchMenu('contact')}}>Contact</button>
+                <div onClick={()=>{setswitchMenu('contact')}} className='flex flex-row items-center cursor-pointer'>
+                    <HelpIcon className='h-[1.5rem] mr-2'/>
+                    <h1 className='md:flex hidden'>Contact</h1>
                 </div>
                 <div className={`h-1 ${switchMenu === 'contact' ? 'bg-white' : 'bg-transparent'} mt-1`}/>
             </div>
