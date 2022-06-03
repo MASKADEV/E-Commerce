5a73178b9461ad261e4f5b2c3067fe05e4f5aaf4
Youness <sidkiyouness@gmail.com>
2022-06-03 21:33:35 +0100

    scripts for check if the user has admin role

--- app/src/pages/Admin/Dashboard.tsx
+++ app/src/pages/Admin/Dashboard.tsx
@@ -1,9 +1,13 @@
+import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import Contact from '../Contact';
 import DashboardNavBar from './componnents/navigation/DashboardNavBar';
 import Home from './sub_screens/Home';
 import Managment from './sub_screens/Managment';
 import Orders from './sub_screens/Orders';
+import GlobalVarialble from '../../config/Constant';
+import { useNavigate } from 'react-router-dom';
+import useAuth from '../../hooks/useAuth';
 
 const Dashboard = () => {
   const [switchMenu, setswitchMenu] = useState<string>('managment');
@@ -20,6 +24,29 @@ const Dashboard = () => {
         return <Contact />
     }
   }
+  let navigate = useNavigate();
+
+  const CheckAuth = async () => {
+    const auth = useAuth();
+    if(!auth)
+     {return navigate('/', {replace : true});}
+    else {
+      let token = localStorage.getItem('token');
+      const config = {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      };
+      let {data} = await axios.get(GlobalVarialble.url + '/auth/getUserInfo', config)
+      if(data['role'] === 0){
+        return navigate('/', {replace : true});
+      }
+    } 
+  }
+
+  useEffect(() => {
+    CheckAuth();
+  },[])
 
   useEffect(() => {
     loadpage(switchMenu);
