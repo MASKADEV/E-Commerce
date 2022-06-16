3a6764dc6fe7780acffdf224fd8a385b5787b951
maskadev <sidkiyouness@gmail.com>
2022-06-16 22:30:46 +0100

    products details page has been added!

--- app/src/pages/product_page/product_details.tsx
+++ app/src/pages/product_page/product_details.tsx
@@ -1,11 +1,14 @@
+/* eslint-disable jsx-a11y/alt-text */
 import axios from 'axios';
-import React, { useEffect, useState } from 'react'
-import { useLocation } from 'react-router-dom'
+import React, { useEffect, useState } from 'react';
 import GlobalVarialble from '../../config/Constant';
+import { useDispatch } from 'react-redux';
+import { addItem } from '../../store/features/addToCartSlice';
+
+
 
 const ProductDetails:React.FC = () => {
     
-    const location = useLocation();
     const [id, setid] = useState<any>();
     const [loading, setLoading] = useState(false);
     const [product_info, setProductInfo] = useState({
@@ -17,6 +20,15 @@ const ProductDetails:React.FC = () => {
         'stock' : 0,
         'price' : 0,
     });
+    const [currentStock , setCurrentStock ] = useState<number>(product_info.stock);
+    const [quantity , setQuantity ] = useState<number>(0);
+
+    const dispatch = useDispatch();
+    const addtoCart = (e : React.MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault();
+        dispatch(addItem({'title' : product_info.title, 'id':id, 'image' : product_info.image_url, 'category' : product_info.categories, 'price' : product_info.price, 'quantity' : quantity}));
+        setQuantity(0);
+    }
 
     useEffect(() => {
         setid(window.location.href.split('/').pop());
@@ -26,18 +38,20 @@ const ProductDetails:React.FC = () => {
         let _url = GlobalVarialble.url + '/admin/fetchSingleProduct';
         let {data} = await axios.post(_url, {'id' : productID});
         setProductInfo({...data});
-        console.log(product_info);
+        setCurrentStock(product_info.stock);
+
     }
 
     useEffect(() => {
         setLoading(true);
         fetchProduct(id);
         setLoading(false);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [id])
-    
+
     return (
         <>
-            {loading === true ? <div></div>
+            { loading === true ? <div></div>
             :
             <div className='md:px-[7rem] px-10 bg-navBar-bg h-screen w-screen items-center flex md:flex-row flex-col'>
                 <div className='md:w-[50%] w-full mt-24 md:mt-0'>
@@ -48,12 +62,15 @@ const ProductDetails:React.FC = () => {
                     <h1 className='text-white font-medium md:text-4xl text-md mt-8'>{product_info.title}</h1>
                     <p className='md:text-md text-sm text-white text-opacity-70 mt-4'>{product_info.description}</p>
                     <p className='my-4 text-white font-bold  text-3xl'>{product_info.price}$</p>
-                    <div className='w-[8rem] flex flex-row justify-between items-center mt-4 text-white'>
-                        <button className='rounded bg-white h-[3rem] w-[3rem] text-main-color text-center text-2xl'>-</button>
-                        <p className='mx-4'>0</p>
-                        <button className='bg-white rounded h-[3rem] w-[3rem] text-main-color text-center text-2xl'>+</button>
+                    <div className='flex flex-row items-center '>
+                        <div className='w-[8rem] h-full flex flex-row justify-center items-center text-white'>
+                            <button onClick={(e:React.MouseEvent<HTMLButtonElement>) => {e.preventDefault(); if(quantity > 0) {setQuantity( quantity -1)}}} className='rounded bg-white h-[2.5rem] w-[2.5rem] text-main-color text-center text-2xl'>-</button>
+                            <p className='mx-4'>{quantity}</p>
+                            <button onClick={(e:React.MouseEvent<HTMLButtonElement>) => {e.preventDefault(); if(product_info.stock > quantity) {setQuantity( quantity +1)}}} className='bg-white rounded h-[2.5rem] w-[2.5rem] text-main-color text-center text-2xl'>+</button>
+                        </div>
+                        <p className='text-white text-md text-opacity-70 ml-3'>{currentStock} Pieces available</p>
                     </div>
-                    <button className='bg-main-color py-3 rounded-md text-white hover:bg-white hover:text-main-color duration-500 mt-11 shadow-lg'>Add To Cart</button>
+                    <button onClick={addtoCart} className='bg-main-color py-3 w-[10rem] rounded-md text-white hover:bg-white hover:text-main-color duration-500 mt-11 shadow-lg'>Add To Cart</button>
                 </div>
             </div>    
         }
