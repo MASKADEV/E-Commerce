44748a56d1a43a4677ba502a2087ffa0cad85c1e
maskadev <sidkiyouness@gmail.com>
2022-06-18 00:40:40 +0100

    add order function

--- rest-api/src/controller/UserController.php
+++ rest-api/src/controller/UserController.php
@@ -31,10 +31,61 @@ class UserController {
 		}
     }
 
+    public function search() {
+        ini_set('display_errors', 1);
+		ini_set('display_startup_errors', 1);
+		error_reporting(E_ALL);
+		require_once('src/models/user/userOperation.php');
+		require_once('src/config/Header.php');
+		require_once('src/security/jwt_handler.php');
+        $jwt = new JwtController();
+        $token = $jwt->getToken();
+        // $jwt->verification($token);
+        if($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $data = json_decode(file_get_contents("php://input"));
+            $search = $data->search;
+            $useropeartion = new UserOperation();
+            $result = $useropeartion->searchProducts($search);
+            echo json_encode($result);
+		} else {
+			echo json_encode(['request' => 'invalide token please try to login again!']);
+		}
+    }
+
+    public function order() {
+        ini_set('display_errors', 1);
+		ini_set('display_startup_errors', 1);
+		error_reporting(E_ALL);
+		require_once('src/models/user/userOperation.php');
+		require_once('src/config/Header.php');
+		require_once('src/security/jwt_handler.php');
+        $jwt = new JwtController();
+        $token = $jwt->getToken();
+        $better_token = md5(uniqid(mt_rand(), true));
+        if($jwt->verification($token)){
+            $result = $jwt->verification($token);
+            if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+                $data = json_decode(file_get_contents("php://input"));
+                $quantity = $data->quantity;
+                $order_id = $data->order_id;
+                $product_id = $data->product_id;
+                $user_id = $result->id;
+                $useropeartion = new UserOperation();
+                $result = $useropeartion->order($product_id, $order_id, $quantity, $user_id);
+                echo json_encode($result);
+            
+            }
+		} else {
+			echo json_encode(['request' => 'invalide token please try to login again!']);
+		}
+
+    }
+
     public static function message($content, $status) {
 	    return json_encode(array(
             'status' => $status, 
             'body' => $content
             ));
 	}
+
  }
--- rest-api/src/models/user/userOperation.php
+++ rest-api/src/models/user/userOperation.php
@@ -1,6 +1,7 @@
 <?php
 
 class UserOperation {
+
     public function updateUserInfo($id, $full_name, $email, $address) {
         require_once('src/models/connection.php');
         $db = new Database();
@@ -8,4 +9,28 @@ class UserOperation {
         $query = $db->connection()->prepare($str);
         $query->execute([$full_name, $email, $address, $id]);
     }
+
+    public function searchProducts($search) {
+        require_once('src/models/connection.php');
+        $db = new Database();
+        // echo $search . ' asdasd';die;
+        $str = 'SELECT * FROM `products` WHERE `title` LIKE ? ';
+        $query = $db->connection()->prepare($str);
+        $query->execute(["%".$search."%"]);
+        $result = $query->fetchAll(PDO::FETCH_ASSOC);
+        return $result;
+    }
+
+    public function order($product_id, $order_id, $quantity, $user_id) {
+        require_once('src/models/connection.php');
+        // echo $product_id . ' ' . $order_id . ' ' . $quantity . ' ' . ' ' . $user_id;die;
+        $db = new Database();
+        $str = 'INSERT INTO `Orders` (`order_id`, `quantity`, `product_id`, `user_id`) VALUES (?, ?, ?, ?)';
+        $query = $db->connection()->prepare($str);
+        $query->execute([$order_id, $quantity, $product_id, $user_id]);
+        // $query->execute([]);
+        $result = $query->fetchAll(PDO::FETCH_ASSOC);
+        return $result;
+    }
+
 }
