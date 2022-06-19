b1295c0eba3147e81c4ad1c93cd36183234ef369
maskadev <sidkiyouness@gmail.com>
2022-06-19 15:39:36 +0100

    specific which page we are in in sub navigation for profile page

--- app/src/pages/Profile/components/navigation.tsx
+++ app/src/pages/Profile/components/navigation.tsx
@@ -6,23 +6,25 @@ interface props {
 }
 
 const ProfileNavbar:React.FC<props> = ({setCurrentPage, currentPage}) => {
+    
     const logout = () => {
         localStorage.removeItem('token');
         window.location.reload();
-      }
+    }
+
   return (
     <div className=' text-white md:w-[60%] w-full mx-auto mt-11'>
         <ul className='flex flex-row justify-between items-center'>
             <li>
                 <div>
-                    <button onClick={() => {setCurrentPage('profileDetails')}}>Profile Details</button>
-                    <hr className={`${currentPage === 'profileDetails' ? 'bg-white' : 'bg-transparent'}`}/>
+                    <button onClick={() => {setCurrentPage('profileDetails')}} className="text-white font-medium md:text-xl text-sm">Profile Details</button>
+                    <hr className={`h-1 ${currentPage === 'profileDetails' ? 'bg-white' : 'bg-transparent'}`}/>
                 </div>
             </li>
             <li>
                 <div>
-                    <button onClick={() => {setCurrentPage('orders')}}>Orders</button>
-                    <hr className={`${currentPage === 'orders' ? 'bg-white' : 'bg-transparent'}`}/>
+                    <button onClick={() => {setCurrentPage('orders')}} className="text-white font-medium md:text-xl text-sm">Orders</button>
+                    <hr className={`h-1 ${currentPage === 'orders' ? 'bg-white' : 'bg-transparent'}`}/>
                 </div>
             </li>
             <li>
