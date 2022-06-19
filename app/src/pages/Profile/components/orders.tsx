ae3e746e77d07aa06339a073c542d26cbbad3c7e
maskadev <sidkiyouness@gmail.com>
2022-06-19 15:39:03 +0100

    add orders for users

--- app/src/pages/Profile/components/orders.tsx
+++ app/src/pages/Profile/components/orders.tsx
@@ -1,10 +1,53 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import GlobalVarialble from '../../../config/Constant';
+import Pagination from '../../Admin/componnents/navigation/ManagmentPagination';
+import OrderTable from '../../Admin/componnents/ui/ordersTable';
+
+const OrdersUser = () => {
 
 
-const Orders:React.FC = () => {
+  const [isLoading, setLoading] = useState<any>(false);
+  const [products, setproducts] = useState<any>([{}]);
+  const [productPerPage] = useState<number>(10);
+  const [currentPage, setcurrentPage] = useState<number>(1);
+  const indexOfLastProduct = currentPage * productPerPage;
+  const indexOfFirstProduct = indexOfLastProduct - productPerPage;
+  const currentProduct = products.slice(indexOfFirstProduct, indexOfLastProduct);
+  const paginate = (pageNumber:number) => setcurrentPage(pageNumber);
+
+  let fetchOrders = async () => {
+    setLoading(true);
+    try{
+      let token = localStorage.getItem('token');
+      const config = {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      };
+      let url = GlobalVarialble.url + '/user/fetchOrders';
+      await axios.get(url, config).then((response : any) => {
+        setproducts(response.data)
+      });
+    }catch(err)
+    {
+      console.log(err);
+    }
+    setLoading(false);
+    };
+
+
+
+  useEffect(() => {
+      fetchOrders();
+  }, [])
+  
   return (
-    <div className='text-white'>Orders</div>
+   <>
+    <OrderTable products={currentProduct} />
+    <Pagination productPerPage={productPerPage} products={products.length} paginate={paginate}/>
+   </>
   )
 }
 
-export default Orders
+export default OrdersUser
--- app/src/pages/Profile/index.tsx
+++ app/src/pages/Profile/index.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import ProfileNavbar from './components/navigation';
-import Orders from './components/orders';
+import OrdersUser from './components/orders';
 import ProfileDetails from './components/profile';
 
 const Profile:React.FC = () => {
@@ -12,7 +12,7 @@ const Profile:React.FC = () => {
       case 'profileDetails':
         return <ProfileDetails />
       case 'orders':
-        return <Orders/>
+        return <OrdersUser/>
       default:
         return <ProfileDetails />
     }
