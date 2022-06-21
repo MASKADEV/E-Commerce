edb7d95f02aa60ec44211fd8efbc3f819e2b201b
maskadev <sidkiyouness@gmail.com>
2022-06-21 18:02:25 +0100

    add blud effect to authentication

--- app/src/pages/authentication/index.tsx
+++ app/src/pages/authentication/index.tsx
@@ -7,7 +7,7 @@ const Authentication:React.FC<AuthProps> = ({showAuth, setAuth}) => {
 
   const [show, setshow] = useState<boolean>(false);
   return (
-    <div className='fixed z-20 top-0 left-0 h-[100%] w-[100%] flex items-center justify-center'>
+    <div className='fixed z-20 top-0 left-0 h-[100%] w-[100%] flex items-center justify-center backdrop-blur-md'>
       {
         show === false ? <Signin show ={show} showAuth={showAuth} setAuth={setAuth} setShow={setshow}/> : <Signup show ={show}  showAuth={showAuth} setAuth={setAuth}  setShow={setshow}/>
       }
