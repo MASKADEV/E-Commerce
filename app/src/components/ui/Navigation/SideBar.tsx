901a6ce020bbb7beece6ce0ab232febb06f4413c
maskadev <sidkiyouness@gmail.com>
2022-06-21 18:00:57 +0100

    disable trending products page

--- app/src/components/ui/Navigation/SideBar.tsx
+++ app/src/components/ui/Navigation/SideBar.tsx
@@ -14,7 +14,7 @@ interface props {
 const SideBar:React.FC<props> = ({currentPage, setcurrentPage}) => {
   return (
    <div className='md:w-[20rem] md:h-full h-[5rem] md:z-0 z-20 w-full bg-sideBar-bg overflow-y-hidden md:top-0 bottom-0 fixed flex flex-col justify-between'>
-        <nav className=' text-white flex md:flex-col flex-row md:items-start items-center md:mt-[6rem] first-letter:items-center h-full md:justify-start justify-between md:mx-0 md:px-0 px-4'>
+        <nav className=' text-white flex md:flex-col flex-row md:items-start items-center md:mt-[6rem] first-letter:items-center h-full md:justify-start justify-between md:mx-0 md:px-0 px-[5rem]'>
             <button onClick={(e : React.MouseEvent<HTMLButtonElement>) => {e.preventDefault();setcurrentPage('latestproducts')}}>
               <div className='flex flex-row items-center md:mr-[2rem] mr-1 md:h-[3rem] md:hover:bg-hover-bg md:w-full'>
                 <HomeIcon className='md:mr-5 mr-2 md:ml-[2rem] md:h-[20px] h-[25px]'/>
@@ -27,12 +27,12 @@ const SideBar:React.FC<props> = ({currentPage, setcurrentPage}) => {
                   <h1 className='md:flex hidden'>Explore Products</h1>
                 </div>
               </button>
-            <button onClick={(e : React.MouseEvent<HTMLButtonElement>) => {e.preventDefault();setcurrentPage('popularProducts')}}>
+            {/* <button onClick={(e : React.MouseEvent<HTMLButtonElement>) => {e.preventDefault();setcurrentPage('popularProducts')}}>
               <div className='flex flex-row items-center md:mr-[2rem] mr-1 md:h-[3rem] md:hover:bg-hover-bg md:w-full md:mt-[5px]'>
                   <DiscoverIcon className='md:mr-5 mr-2 md:h-[20px] h-[25px] md:ml-[2rem]'/>
                   <h1 className='md:flex hidden'>Popular Products</h1>
               </div>
-            </button>
+            </button> */}
         </nav>
         <div className=' md:flex hidden flex-col items-center'>
           <div className='h-[2px] w-[95%] mx-4 bg-gray mb-4'></div>
