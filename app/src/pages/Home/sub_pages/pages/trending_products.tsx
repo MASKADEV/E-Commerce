30d5929fb755346b652ebdba40597b4d3f6a7577
maskadev <sidkiyouness@gmail.com>
2022-06-21 18:03:48 +0100

    change trending product and latest product page folder

---
+++ app/src/pages/Home/sub_pages/pages/trending_products.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const TrendingProducts = () => {
+    return (
+        <div className='flex flex-row overflow-x-hidden w-full'>
+          <div className='md:grid md:grid-cols-2 xl:grid-cols-3 md:gap-5 mb-3 '>
+]          </div>
+        </div>
+    )
+}
+
+export default TrendingProducts
