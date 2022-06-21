b671de5adc41a570159e3342b0113886dd663069
maskadev <sidkiyouness@gmail.com>
2022-06-21 18:02:04 +0100

    add message components

--- app/src/pages/Admin/sub_screens/Message.tsx
+++ app/src/pages/Admin/sub_screens/Message.tsx
@@ -2,7 +2,114 @@ import React from 'react'
 
 const Message = () => {
   return (
-    <div>Message</div>
+    <div className="container p-2 mx-auto sm:p-4 text-white md:pt-[10rem] pt-[4rem]">
+      <div className="flex flex-col overflow-x-auto text-xs">
+        <div className="flex text-left ">
+          <div className="flex items-center justify-center w-8 px-2 py-3 sm:p-3">
+            <input type="checkbox" name="All" className="w-3 h-3 rounded-sm accent-violet-400" />
+          </div>
+          <div className="w-32 px-2 py-3 sm:p-3">Sender</div>
+          <div className="flex-1 px-2 py-3 sm:p-3">Message</div>
+          <div className="hidden w-24 px-2 py-3 text-right sm:p-3 sm:block">Received</div>
+        </div>
+        <div className="flex border-b border-opacity-20  ">
+          <div className="flex items-center justify-center w-8 px-2 py-3 sm:p-3">
+            <input type="checkbox" className="w-3 h-3 rounded-sm accent-violet-400" name="Box0" />
+          </div>
+          <div className="w-32 px-2 py-3 sm:p-3">
+            <p>Tracy Kim</p>
+          </div>
+          <div className="flex-1 block px-2 py-3 truncate sm:p-3 sm:w-auto">An cum minimum voluptua repudiandae, nec illum essent et. Id tibique voluptatum per, eos eu civibus mnesarchum intellegam. An mundi detracto usu, diceret deserunt lobortis te cum.</div>
+          <div className="hidden w-24 px-2 py-3 text-right sm:p-3 sm:block ">
+            <p>5min ago</p>
+          </div>
+        </div>
+        <div className="flex border-b border-opacity-20  ">
+          <div className="flex items-center justify-center w-8 px-2 py-3 sm:p-3">
+            <input type="checkbox" className="w-3 h-3 rounded-sm accent-violet-400" name="Box1" />
+          </div>
+          <div className="w-32 px-2 py-3 sm:p-3">
+            <p>Katherine Rojas</p>
+          </div>
+          <div className="flex-1 block px-2 py-3 truncate sm:p-3 sm:w-auto">At nam diceret intellegam, diam delicatissimi eu nam. Vivendo adolescens dissentiet cum ex, fuisset percipit eum ne. Unum officiis invenire vix ex.</div>
+          <div className="hidden w-24 px-2 py-3 text-right sm:p-3 sm:block ">
+            <p>37min ago</p>
+          </div>
+        </div>
+        <div className="flex border-b border-opacity-20  ">
+          <div className="flex items-center justify-center w-8 px-2 py-3 sm:p-3">
+            <input type="checkbox" className="w-3 h-3 rounded-sm accent-violet-400" name="Box2" />
+          </div>
+          <div className="w-32 px-2 py-3 sm:p-3">
+            <p>Santiago Mcguire</p>
+          </div>
+          <div className="flex-1 block px-2 py-3 truncate sm:p-3 sm:w-auto">Quis graeci ex mea. Sed ea odio similique. Enim porro persius et usu, et sea partem melius sententiae, mea viderer fuisset id.</div>
+          <div className="hidden w-24 px-2 py-3 text-right sm:p-3 sm:block ">
+            <p>1h ago</p>
+          </div>
+        </div>
+        <div className="flex border-b border-opacity-20  ">
+          <div className="flex items-center justify-center w-8 px-2 py-3 sm:p-3">
+            <input type="checkbox" className="w-3 h-3 rounded-sm accent-violet-400" name="Box3" />
+          </div>
+          <div className="w-32 px-2 py-3 sm:p-3">
+            <p>Keaton Collins</p>
+          </div>
+          <div className="flex-1 block px-2 py-3 truncate sm:p-3 sm:w-auto">Vix an ullum fuisset, ad nominati intellegat signiferumque qui, vidit dolores rationibus id est. Quo ei assum option, ludus invenire dignissim et mea.</div>
+          <div className="hidden w-24 px-2 py-3 text-right sm:p-3 sm:block ">
+            <p>3h ago</p>
+          </div>
+        </div>
+        <div className="flex border-b border-opacity-20  ">
+          <div className="flex items-center justify-center w-8 px-2 py-3 sm:p-3">
+            <input type="checkbox" className="w-3 h-3 rounded-sm accent-violet-400" name="Box4" />
+          </div>
+          <div className="w-32 px-2 py-3 sm:p-3">
+            <p>Ana Stevenson</p>
+          </div>
+          <div className="flex-1 block px-2 py-3 truncate sm:p-3 sm:w-auto">Vis sanctus gubergren abhorreant ea, eu melius percipit legendos mei, vulputate temporibus qui ut. Eu eam sumo putent discere, id vim feugiat lobortis democritum, fabellas invidunt mnesarchum at duo.</div>
+          <div className="hidden w-24 px-2 py-3 text-right sm:p-3 sm:block ">
+            <p>8h ago</p>
+          </div>
+        </div>
+        <div className="flex border-b border-opacity-20  ">
+          <div className="flex items-center justify-center w-8 px-2 py-3 sm:p-3">
+            <input type="checkbox" className="w-3 h-3 rounded-sm accent-violet-400" name="Box5" />
+          </div>
+          <div className="w-32 px-2 py-3 sm:p-3">
+            <p>Elliott Mejia</p>
+          </div>
+          <div className="flex-1 block px-2 py-3 truncate sm:p-3 sm:w-auto">Pri habeo eligendi at, ex quidam probatus sed, ei cum mentitum mnesarchum. Ex est purto congue. Unum soluta tritani in eam, habemus percipit vim an. Duo ad tota voluptaria dissentias, an eum bonorum maiestatis, alia habeo imperdiet ex vix.</div>
+          <div className="hidden w-24 px-2 py-3 text-right sm:p-3 sm:block ">
+            <p>yesterday</p>
+          </div>
+        </div>
+        <div className="flex border-b border-opacity-20  ">
+          <div className="flex items-center justify-center w-8 px-2 py-3 sm:p-3">
+            <input type="checkbox" className="w-3 h-3 rounded-sm accent-violet-400" name="Box6" />
+          </div>
+          <div className="w-32 px-2 py-3 sm:p-3">
+            <p>Riley Nunez</p>
+          </div>
+          <div className="flex-1 block px-2 py-3 truncate sm:p-3 sm:w-auto">Con no error impedit definitionem, qui sanctus detraxit vulputate id. Et natum eligendi moderatius vis. Pro ea mazim nominati delicata, per ad melius vituperatoribus. Ad dolor signiferumque nam, eam justo adipisci argumentum ut.</div>
+          <div className="hidden w-24 px-2 py-3 text-right sm:p-3 sm:block ">
+            <p>2 days ago</p>
+          </div>
+        </div>
+        <div className="flex border-b border-opacity-20  ">
+          <div className="flex items-center justify-center w-8 px-2 py-3 sm:p-3">
+            <input type="checkbox" className="w-3 h-3 rounded-sm accent-violet-400" name="Box7" />
+          </div>
+          <div className="w-32 px-2 py-3 sm:p-3">
+            <p>Garret Ramsey</p>
+          </div>
+          <div className="flex-1 block px-2 py-3 truncate sm:p-3 sm:w-auto">Justo homero similique eu his, oratio libris et vix. Eu eruditi conceptam cum, ius id cibo impetus consectetuer, lorem everti contentiones pri ne. Aperiam utroque pri ex, eu sit quodsi quaestio persecuti. </div>
+          <div className="hidden w-24 px-2 py-3 text-right sm:p-3 sm:block ">
+            <p>4 days ago</p>
+          </div>
+        </div>
+      </div>
+</div>
   )
 }
 
